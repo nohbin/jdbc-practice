83d68c3a8ec3903104df31c7c8819af4349c80eb
Nohbin <nohbinmusic@gmail.com>
2023-08-02 00:16:40 +0900

    -- Jdbc TEST

---
+++ src/test/resources/db_schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS USERS;
+
+CREATE TABLE USERS(
+    userId varchar(12) not null ,
+    password varchar(12) not null ,
+    name varchar(20) not null ,
+    email varchar(50),
+
+    PRIMARY KEY (userId)
+);
